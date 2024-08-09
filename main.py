f4aff9103f91011aa9a6298b6c9067bc60518b2b
brunameinberg <brunalm2@al.insper.edu.br>
2024-08-09 11:32:13 -0300

    pronto e comentado

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import sys
 def calcula(arg):
     #removendo os espaços
     arg = arg.replace(" ", "")
-    arg_orginial = arg
-    #pegando os sinais
-    indice_mais = []
-    indice_menos = []
-    indice_sinais = []
-    lista_sinais = []
+    #pegando os dados dos sinais
+    indice_mais = [] #indice de onde estão os sinais de mais'
+    indice_menos = [] #indice de onde estão os sinais de menos
+    indice_sinais = [] #indice de onde estão os sinais
+    lista_sinais = [] #lista com os sinais
+
     i = 0 
     while i in range(len(arg)):
         if arg[i] == "+":
@@ -23,16 +23,19 @@ def calcula(arg):
 
         i = i + 1
 
+    #construindo a lista de números
     arg = arg.replace("+", " ")
     arg = arg.replace("-", " ")
     lista_numeros = arg.split(" ")
 
+    #lista com os indices dos números
     indice_numeros = []
 
     for j in range(len(lista_numeros) + len(indice_sinais)):
         if j not in indice_sinais:
             indice_numeros.append(j)
 
+    #checando se o primeiro elemento é um número ou um sinal
     primeiro_e_numero = False
        
     if indice_sinais[0] < indice_numeros[0]:
@@ -41,25 +44,9 @@ def calcula(arg):
         primeiro_e_numero = True
         lista_sinais.insert(0, "+")
 
-    primeiro_e_mais = True
-
-    if indice_mais[0] < indice_menos[0]:
-        primeiro_e_mais = True
-    else:
-        primeiro_e_mais = False
-
-    primeiro_e_sinal = False
-
-    if indice_sinais[0] < indice_numeros[0]:
-        primeiro_e_sinal = True
-    else:
-        primeiro_e_sinal = False
 
     expressao = 0
-    primeira_rodada = True
-
     for k in range (len(lista_numeros)):
-        #print(expressao)
 
         if lista_sinais[k] == "+":
             expressao = expressao + int(lista_numeros[k])
